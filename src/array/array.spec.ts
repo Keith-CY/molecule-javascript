84a317c2ce9e990a83ba30e8296527242477af7d
Keith <keithwhisper@gmail.com>
2020-01-20 15:01:55 +0800

    feat: add the method of deserializing array

--- src/array/array.spec.ts
+++ src/array/array.spec.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/extensions */
-import { serializeArray } from '.'
+import { serializeArray, deserializeArray } from '.'
 
-const { serialize: serializeFixture } = require('./fixture.json')
+const { serialize: serializeFixture, deserialize: deserializeFixture } = require('./fixture.json')
 
 describe('Test serialize array', () => {
   const fixtureTable: [
@@ -22,6 +22,33 @@ describe('Test serialize array', () => {
     } else {
       const actual = serializeArray(source)
       expect(actual).toBe(expected)
+    }
+  })
+})
+
+describe('Test deserialize array', () => {
+  const fixtureTable: [
+    { serialized: string; sizes: number[] },
+    string[] | undefined,
+    string | undefined,
+  ][] = deserializeFixture.map(
+    ({
+      source,
+      expected,
+      exception,
+    }: {
+      source: { serialized: string; sizes: number[] }
+      expected?: string[]
+      exception?: string
+    }) => [source, expected, exception],
+  )
+
+  test.each(fixtureTable)(`%s => %s ? %s`, (source, expected, exception) => {
+    if (exception) {
+      expect(() => deserializeArray(source.serialized, source.sizes)).toThrow(exception)
+    } else {
+      const actual = deserializeArray(source.serialized, source.sizes)
+      expect(actual).toEqual(expected)
     }
   })
 })
--- src/array/index.ts
+++ src/array/index.ts
@@ -4,7 +4,35 @@ export const serializeArray = (array: HexString[]) => {
   array.forEach(item => {
     assertIsHexStr(item)
   })
-  return `0x${array.map(item => item.slice(2)).join('')}`
+  return `0x${array.map(item => (item.length % 2 ? `0${item.slice(2)}` : item.slice(2))).join('')}`
 }
 
-export default { serializeArray }
+export const deserializeArray = (serialized: string, sizes: number[]) => {
+  assertIsHexStr(serialized)
+  if (!Array.isArray(sizes) || !sizes.length) {
+    throw new TypeError(`Expect sizes to be an array of size`)
+  }
+  sizes.forEach(size => {
+    if (typeof size !== 'number') {
+      throw new TypeError('Expect size to be a number')
+    }
+  })
+  const checkpoints: number[] = sizes.reduce(
+    (points: number[], size: number) => {
+      return [...points, points[points.length - 1] + size]
+    },
+    [0],
+  )
+
+  const raw = serialized.length % 2 ? `0${serialized.slice(2)}` : serialized.slice(2)
+
+  if (checkpoints[checkpoints.length - 1] > raw.length) {
+    throw new Error('Size exceeds the serialized data')
+  }
+
+  return checkpoints.slice(1).map((point, idx) => {
+    return `0x${raw.slice(checkpoints[idx], point)}`
+  })
+}
+
+export default { serializeArray, deserializeArray }
