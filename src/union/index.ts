a9c6d6f03b495bf81f92bac7ff61733bcb260e44
duanyytop <duanyytop@gmail.com>
2020-01-22 11:58:43 +0800

    feat: implement union serialize and deserialize

--- src/union/index.ts
+++ src/union/index.ts
@@ -1,1 +1,25 @@
-export default {}
+import { assertIsHexStr, uint32Le } from '../utils'
+
+export const serializeUnion = (origin: UnionElement[]) => {
+  const values = origin.map(item => item[2])
+  if (values.length !== 1) {
+    throw Error('Expect the count of union element to be 1')
+  }
+  assertIsHexStr(values[0])
+  const indices = origin.map(item => item[1])
+  return uint32Le(`0x${indices[0].toString(16)}`) + values[0].slice(2)
+}
+
+export const deserializeUnion = (serialized: string, indices: UnionIndex[]) => {
+  assertIsHexStr(serialized)
+  if (indices.length !== 1) {
+    throw Error('Expect the count of union element to be 1')
+  }
+  if (serialized.slice(0, 10) !== uint32Le(`0x${indices[0][1].toString(16)}`)) {
+    throw Error('Expect serialized index to be equal to first indices element')
+  }
+  const value = `0x${serialized.slice(10)}`
+  return [[indices[0][0], indices[0][1], value]]
+}
+
+export default { serializeUnion, deserializeUnion }
--- types/global.d.ts
+++ types/global.d.ts
@@ -2,6 +2,8 @@ declare type Byte = string
 declare type HexString = string
 declare type TypeElement = [string, string]
 declare type TypeSize = [string, number]
+declare type UnionElement = [string, number, string]
+declare type UnionIndex = [string, number]
 
 declare module '*.json' {
   const value: string
