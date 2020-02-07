0a8282739da9568b414ba237ad91798d5c3b4ac8
Keith <keithwhisper@gmail.com>
2020-02-07 12:35:44 +0800

    feat: bundle the package

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'lib'),
+    libraryTarget: 'umd',
+    library: 'MoleculeJavaScript',
+    globalObject: 'globalThis',
+  },
+}
