305d83045bc7952257d91e2fcedd9717e6d60c5a
Keith <keithwhisper@gmail.com>
2020-02-13 17:42:25 +0800

    feat: work with moleculec

--- src/cli.ts
+++ src/cli.ts
@@ -1,16 +1,33 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 import fs from 'fs'
+import readline from 'readline'
 
 const [, , ...args] = process.argv
 
-const schemaOrPath = args[0]
+let schemaOrPath = args[0]
 const outputFile = args[1] || ''
 
-const handleSchema = () => {
+const getFormat = () => {
+  if (schemaOrPath === '--format') {
+    console.info('JSON')
+    process.exit(0)
+  }
+}
+
+const handleInput = () => {
+  getFormat()
   try {
     if (fs.existsSync(outputFile)) {
       fs.unlinkSync(outputFile)
+    }
+
+    if (!fs.existsSync(schemaOrPath)) {
+      try {
+        JSON.parse(schemaOrPath)
+      } catch {
+        throw new Error(`Expect schema to be a JSON`)
+      }
     }
     const normalizeScript = fs.existsSync(schemaOrPath)
       ? `const normalizedSchema = Schema.fromFile('${schemaOrPath}').getNormalizedSchema()`
@@ -37,10 +54,28 @@ module.exports = { normalizedSchema, molecules }
 }
 
 if (!schemaOrPath) {
-  console.error('Please use command as molecule-javascript <schema | file path> [outputFile]')
-  process.exit(1)
+  let input = ''
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  rl.on('line', line => {
+    input += line
+  })
+    .on('close', () => {
+      schemaOrPath = input
+      if (!schemaOrPath) {
+        console.error('Please use command as moleculec-javascript <schema | file path> [outputFile]')
+        process.exit(1)
+      }
+      handleInput()
+    })
+    .on('error', err => {
+      console.error(err)
+      process.exit(1)
+    })
 } else {
-  handleSchema()
+  handleInput()
 }
 
 export default undefined
