8001730d634d5db897402490db52ed3a415d26a0
Keith <keithwhisper@gmail.com>
2020-02-18 18:02:43 +0800

    feat: update utils for duktape

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
+const padStart = (str: string, len: number, char: string) => {
+  return (char.repeat(len) + str).slice(-1 * len)
+}
+
 export function assertIsHexStr(str: any): asserts str is HexString {
   if (typeof str !== 'string' || !str.startsWith('0x') || (Number.isNaN(+str) && str !== '0x')) {
     throw new TypeError(`Expect ${str} to be a hex string`)
@@ -7,20 +11,22 @@ export function assertIsHexStr(str: any): asserts str is HexString {
 export const uint16Le = (uint16: string) => {
   assertIsHexStr(uint16)
   const buf = new ArrayBuffer(2)
-  new DataView(buf).setUint16(0, +uint16, true)
-  return `0x${[...new Uint8Array(buf)].map(v => v.toString(16).padStart(2, '0')).join('')}`
+  const dv = new DataView(buf)
+  dv.setUint16(0, +uint16, true)
+  return `0x${padStart(dv.getUint16(0, false).toString(16), 4, '0')}`
 }
 
 export const uint32Le = (uint32: string) => {
   assertIsHexStr(uint32)
   const buf = new ArrayBuffer(4)
-  new DataView(buf).setUint32(0, +uint32, true)
-  return `0x${[...new Uint8Array(buf)].map(v => v.toString(16).padStart(2, '0')).join('')}`
+  const dv = new DataView(buf)
+  dv.setUint32(0, +uint32, true)
+  return `0x${padStart(dv.getUint32(0, false).toString(16), 8, '0')}`
 }
 
 export const uint64Le = (uint64: string) => {
   assertIsHexStr(uint64)
-  const val = uint64.slice(2).padStart(16, '0')
+  const val = padStart(uint64.slice(2), 16, '0')
   const valLeft = `0x${val.slice(0, 8)}`
   const valRight = `0x${val.slice(8)}`
   const viewLeft = uint32Le(valLeft).slice(2)
